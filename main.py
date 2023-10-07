2ebb6dea4b610730c0ee3434ae55d7e90bf2d7ef
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-07 12:06:36 +0530

    add like router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.routers.user import user_router
 from src.routers.post import post_router
 from src.routers.comment import comment_router
+from src.routers.like import like_router
 
 app = FastAPI()
 
@@ -9,3 +10,4 @@ app = FastAPI()
 app.include_router(user_router, tags=["user"])
 app.include_router(post_router, tags=["post"])
 app.include_router(comment_router, tags=["comment"])
+app.include_router(like_router, tags=["like"])
