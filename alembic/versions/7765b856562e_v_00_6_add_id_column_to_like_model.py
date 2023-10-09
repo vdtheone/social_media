4c08fad474f95e4630b8e3d429d7deb0c04c7b1f
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-09 12:56:38 +0530

    add id column in Like table

--- alembic/versions/7765b856562e_v_00_6_add_id_column_to_like_model.py
+++ alembic/versions/7765b856562e_v_00_6_add_id_column_to_like_model.py
@@ -1,8 +1,8 @@
-"""V_00_3_add_relationship
+"""V_00_6_add id column to Like model
 
-Revision ID: a64fe832e68a
-Revises: 17b55276da0f
-Create Date: 2023-10-06 22:42:59.570188
+Revision ID: 7765b856562e
+Revises: 85145e89d339
+Create Date: 2023-10-09 12:42:38.003161
 
 """
 from typing import Sequence, Union
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'a64fe832e68a'
-down_revision: Union[str, None] = '17b55276da0f'
+revision: str = '7765b856562e'
+down_revision: Union[str, None] = '85145e89d339'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
