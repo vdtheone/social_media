7bdb9131987530aa461a75359959515a35f83be5
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-09 12:52:07 +0530

    add id column

--- src/models/like.py
+++ src/models/like.py
@@ -1,17 +1,25 @@
 from datetime import datetime
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 from src.config import Base
 
 
+# class Like(Base):
+#     __tablename__ = "likes"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     is_liked = Column(Boolean, default=False)
+#     user_id = Column(Integer, ForeignKey("users.id"))
+#     post_id = Column(Integer, ForeignKey("posts.id"))
+
+#     # users = relationship("User", back_populates="likes")
+#     posts = relationship("Post", back_populates='likes')
+
+
 class Like(Base):
-    __tablename__ = "likes"
-
+    __tablename__ = 'likes'
+    
     id = Column(Integer, primary_key=True, index=True)
-    is_liked = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    post_id = Column(Integer, ForeignKey("posts.id"))
-
-    # users = relationship("User", back_populates="likes")
-    posts = relationship("Post", back_populates='likes')
-
+    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id'), primary_key=True)
+    
