6878226cddc1949c1a1451d2bdd52eadb6a60bf3
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-09 12:55:25 +0530

    remove is_liked field from column and chage logic

--- src/services/like.py
+++ src/services/like.py
@@ -9,37 +9,23 @@ def like_post(id:int, request:Request, db:Session):
     userid = get_current_user_id(request)
     check_like = db.query(Like).filter(Like.user_id == userid).first()
 
-    if check_like is not None and check_like.is_liked and check_like.post_id:
-        check_like.is_liked = False
-        db.commit()
-        db.refresh(check_like)
+    if check_like is not None and check_like.post_id:
         # decrese commnet count
         post = db.query(Post).get(id)
         post.number_of_likes-=1
+        db.delete(check_like)
         db.commit()
         db.refresh(post)
         return {"message":"Post Disliked"}
-    
-    elif check_like is not None and check_like.is_liked is False and check_like.post_id:
-        check_like.is_liked = True
-        db.commit()
-        db.refresh(check_like)
-        # increase comment count
-        post = db.query(Post).get(id)
-        post.number_of_likes+=1
-        db.commit()
-        db.refresh(post)
-        return {"message":"Post Liked"}
-    
     else:
         like = Like(
-            is_liked = True,
             user_id = userid,
             post_id = id
         )
         db.add(like)
         db.commit()
         db.refresh(like)
+
         # increse comment count
         post = db.query(Post).get(id)
         post.number_of_likes+=1
