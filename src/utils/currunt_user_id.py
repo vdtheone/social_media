4907a0c5fba73c98f446eafaea85f512cfb7b339
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-10 15:28:45 +0530

    handle error when no token is provided

--- src/utils/currunt_user_id.py
+++ src/utils/currunt_user_id.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, Request
+from fastapi import HTTPException, Request, status
 from jose import jwt, ExpiredSignatureError, JWTError
 import os
 
@@ -7,21 +7,26 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = os.environ.get("ALGORITHM")
 
 
-def get_current_user_id(request:Request): 
+def get_current_user_id(request: Request):
     try:
         access_token = request.headers.get("Authorization")
+        if not access_token:
+            # Handle the case where no access token is provided
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Access token is missing",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
         access_token = access_token.split()[1]
-        user_id = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM]).get('id')
+        user_id = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM]).get("id")
         return user_id
     except ExpiredSignatureError:
-            raise HTTPException(status_code=401, detail="Token Expired")
+        raise HTTPException(status_code=401, detail="Token Expired")
     except JWTError:
         raise HTTPException(status_code=401, detail="Provide valid Token")
     except IndexError:
-        raise HTTPException(
-            status_code=401, detail="list index out of range in token"
-        )
+        raise HTTPException(status_code=401, detail="list index out of range in token")
     except HTTPException as e:
         raise e
     except Exception:
-            raise HTTPException(status_code=500, detail="Internal Server Error")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
